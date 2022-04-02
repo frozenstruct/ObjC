186d4b06ad4dfde68524ce8239a6231e98b75839
David Taylor <david.pbx.taylor@gmail.com>
2022-04-02 12:48:36 +0300

    chapter 2

---
+++ 2OOP/2OOP/Fraction.h
@@ -0,0 +1,23 @@
+//
+//  Fraction.h
+//  2OOP
+//
+//  Created by Dmitry Aksyonov on 01.04.2022.
+//
+
+#ifndef Fraction_h
+#define Fraction_h
+
+#import <Foundation/Foundation.h>
+
+@interface FractionManager : NSObject
+
+-(void) print;
+
+-(void) setNumerator: (int) n;
+
+-(void) setDenominator: (int) d;
+
+@end
+
+#endif /* Fraction_h */
---
+++ 2OOP/2OOP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2OOP
+//
+//  Created by Dmitry Aksyonov on 01.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
